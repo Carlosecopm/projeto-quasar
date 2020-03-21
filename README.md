22200b1b847ce653fbf953256bfe04063b09611e
Carlos Silva <carlosluizecomp.uefs@gmail.com>
2020-03-20 22:41:12 -0300

    Primeiro commit do projeto

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# projeto-quasar
-Testando quasar no notebook
+# Aplicação exemplo (rojeto-quasar)
+
+Aplicação de blog
+
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+npm run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="container">
+    <div class="row">
+      <div class="col-3">
+        <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+       <q-card-actions>
+        <q-btn flat>Action 1</q-btn>
+        <q-btn flat>Action 2</q-btn>
+      </q-card-actions>
+    </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
